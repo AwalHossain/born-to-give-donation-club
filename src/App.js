866306b4e80d5a364ec74d25afeabcc115de1ad4
AwalHossain <waqin21@gmail.com>
2021-09-26 15:44:45 +0600

    backgroung image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import ProductCard from './components/Product/ProductCard';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='body'>
+      <Header></Header>
       <ProductCard></ProductCard>
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand cursive" href="/">Born To Give</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item ms-0">
+         <button className='btn '> <a class="nav-link active " aria-current="page" href="/">Sign Up</a></button>
+        </li>
+        <li class="nav-item ms-0">
+            <button className='btn '>  <a class="nav-link active " aria-current="page" href="/">Home</a></button>
+        
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div className='shadow'>
+<div className='jumbotron '>
+</div>
+<div className='title-part container'>
+        <h1 className='head'>Donate</h1>
+        <h3 className='head-bottom'>For a better world</h3>
+        </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
